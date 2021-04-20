9ac0c60f1b5207a3ecb7668c1bfb6ed7b2fea6a6
Apoorv <apoorvsahay10@gmail.com>
2021-04-21 02:18:27 +0530

    added bottom bar

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
 
+import 'package:custom_bottom_navigation_bar/custom_bottom_navigation_bar.dart';
+import 'package:custom_bottom_navigation_bar/custom_bottom_navigation_bar_item.dart';
+
 class SearchPage extends StatelessWidget {
+  final _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Center(
-        child: Text('Search'),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        onTap: (index) {
+          _pageController.animateToPage(index,
+              curve: Curves.fastLinearToSlowEaseIn,
+              duration: Duration(milliseconds: 600));
+        },
+        items: [
+          CustomBottomNavigationBarItem(
+            icon: Icons.map,
+            title: "Map",
+          ),
+          CustomBottomNavigationBarItem(
+            icon: Icons.near_me,
+            title: "Directions",
+          ),
+          CustomBottomNavigationBarItem(
+            icon: Icons.search,
+            title: "Search",
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          Center(
+            child: Icon(
+              Icons.map,
+               color: Colors.white,
+            ),
+          ),
+          Center(
+            child: Icon(
+              Icons.near_me,
+              color: Colors.white,
+            ),
+          ),
+          Center(
+            child: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
